503ca2d36a9f682b4137142fb360d1bbebe43626
irihorn <irihorn@gmail.com>
2023-04-07 04:10:13 -0400

    Test

---
+++ miac/miac/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from miac.users.models import User
+from miac.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
